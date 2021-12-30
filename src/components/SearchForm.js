39353c2935a87f58f6367dd1e804615ae1d2297f
Christian Siki <christiansiki@Jacquelyns-MacBook-Pro.local>
2021-12-30 15:57:05 -0600

    initial commit

---
+++ src/components/SearchForm.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useGlobalContext } from '../context'
+
+const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef('');
+
+  //set focus on searchbar on initial render
+  React.useEffect(() =>{
+    searchValue.current.focus();
+  },[]);
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value)
+  }
+  
+  //prevent refresh when 'Enter' is hit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+  return (
+    <section className='section search'>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">search your favorite cocktail</label>
+          <input
+             type="text" 
+             id="name" 
+             ref={searchValue} 
+             onChange={searchCocktail} 
+          />
+        </div>
+      </form>
+    </section>
+  )
+}
+
+export default SearchForm
---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Id necessitatibus veniam sint repudiandae quia aperiam, 
+        exercitationem laboriosam maiores saepe nisi accusamus impedit, 
+        neque molestiae ullam ut at ad facere delectus rem, suscipit 
+        laudantium? Reiciendis, voluptate. Tempore totam fugit 
+        nesciunt excepturi!
+      </p>
+    </section>
+  )
+}
+
+export default About
